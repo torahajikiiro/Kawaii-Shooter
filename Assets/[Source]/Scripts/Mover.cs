89d98eb84046e3232bc7e15060fe919ea25445d3
torahajikiiro <54126021+torahajikiiro@users.noreply.github.com>
2019-08-15 21:28:00 +0700

    Intial commit

---
+++ Assets/[Source]/Scripts/Mover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    public float MoveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+        //gameObject.SetActive(false);
+        //enabled = false;
+       
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        if( Input.GetKey(KeyCode.RightArrow)){
+            var newPosition = transform.position;
+            newPosition.x += MoveSpeed;
+            transform.position = newPosition;
+        }else  if( Input.GetKey(KeyCode.LeftArrow)){
+            var newPosition = transform.position;
+            newPosition.x -= MoveSpeed;
+            transform.position = newPosition;
+        }
+        //Debug.Log("Update");
+       
+    }
+
+}
